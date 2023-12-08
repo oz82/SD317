228fa5c1ee41d3cdeb0e1efe5cd50dd4ffeeb2eb
oz82 <oz82@proton.me>
2023-12-08 14:08:55 +0300

    MySet added

---
+++ data_structures/my-data-structure/src/set/MySet.java
@@ -0,0 +1,59 @@
+package set;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class MySet<T> implements Iterable<T> {
+
+    private ArrayList<T> elements;
+
+    public MySet() {
+        this.elements = new ArrayList<>();
+    }
+
+    public int size() {
+        return elements.size();
+    }
+
+    public boolean isEmpty() {
+        return elements.isEmpty();
+    }
+
+    public boolean contains(T element) {
+        return elements.contains(element);
+    }
+
+    public void add(T element) {
+        // bu metodu uygun sekilde yazin.
+    }
+
+    public void remove(T element) {
+        elements.remove(element);
+    }
+
+    public void clear() {
+        elements.clear();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return elements.iterator();
+    }
+
+    public static void main(String[] args) {
+        MySet<String> mySet = new MySet<>();
+        mySet.add("Java");
+        mySet.add("Python");
+        mySet.add("Java");  // Aynı elemanı eklemeye çalıştık, ancak küme olduğu için eklenmeyecek
+
+        System.out.println("MySet size: " + mySet.size());
+        System.out.println("MySet contains 'Java': " + mySet.contains("Java"));
+
+        System.out.println("Elements in MySet:");
+        for (String element : mySet) {
+            System.out.println(element);
+        }
+
+        mySet.remove("Python");
+        System.out.println("After removing 'Python', MySet size: " + mySet.size());
+    }
+}
